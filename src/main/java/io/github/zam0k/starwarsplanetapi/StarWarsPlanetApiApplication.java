db34aeb39068cce1ae6bd49d1c2a6097d50b9400
Kelly Castelo <kawokkun@gmail.com>
2022-09-13 11:11:07 -0300

    initial commit

---
+++ src/main/java/io/github/zam0k/starwarsplanetapi/StarWarsPlanetApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.zam0k.starwarsplanetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsPlanetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsPlanetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/zam0k/starwarsplanetapi/StarWarsPlanetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zam0k.starwarsplanetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsPlanetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
