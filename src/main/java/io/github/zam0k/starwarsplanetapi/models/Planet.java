0fc33e01e5604c5a9580737eb9199ed0866844e8
Kelly Castelo <kawokkun@gmail.com>
2022-09-14 13:14:57 -0300

    format planet class and change length

--- src/main/java/io/github/zam0k/starwarsplanetapi/models/Planet.java
+++ src/main/java/io/github/zam0k/starwarsplanetapi/models/Planet.java
@@ -12,15 +12,19 @@ import javax.persistence.*;
 @AllArgsConstructor
 public class Planet {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(length = 180, nullable = false)
-    private String name;
-    @Column(length = 180, nullable = false)
-    private String climate;
-    @Column(length = 180, nullable = false)
-    private String terrain;
-    @Column(name = "number_of_apparitions", nullable = false)
-    private Integer numberOfApparitions;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 80, nullable = false, unique = true)
+  private String name;
+
+  @Column(length = 30, nullable = false)
+  private String climate;
+
+  @Column(length = 30, nullable = false)
+  private String terrain;
+
+  @Column(name = "number_of_apparitions", nullable = false)
+  private Integer numberOfApparitions;
 }
