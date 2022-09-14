2abf28d9fd0c136b7b5204f991c07e718d57eb74
Kelly Castelo <kawokkun@gmail.com>
2022-09-14 13:15:51 -0300

    add missing validators to CreatePlanetDTO class

--- src/main/java/io/github/zam0k/starwarsplanetapi/controllers/dtos/CreatePlanetDTO.java
+++ src/main/java/io/github/zam0k/starwarsplanetapi/controllers/dtos/CreatePlanetDTO.java
@@ -5,15 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreatePlanetDTO {
-    @NotBlank(message = "name cannot be null")
-    private String name;
-    @NotBlank(message = "climate cannot be null")
-    private String climate;
-    @NotBlank(message = "terrain cannot be null")
-    private String terrain;
+  @NotBlank(message = "name cannot be null")
+  @Size(min = 5, max = 80, message = "name must be between 5 and 80 characters")
+  private String name;
+
+  @NotBlank(message = "climate cannot be null")
+  @Size(min = 3, max = 30, message = "climate must be between 3 and 30 characters")
+  private String climate;
+
+  @NotBlank(message = "terrain cannot be null")
+  @Size(min = 3, max = 30, message = "terrain must be between 3 and 30 characters")
+  private String terrain;
 }
